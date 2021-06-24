736cc1b634f2dd239b780a853f25f7194d5bf567
Nir Kahana <>
2021-06-24 10:44:35 +0300

    folder structure improved, added a few comments

--- wp-content/themes/pokedex/front-page.php
+++ wp-content/themes/pokedex/front-page.php
@@ -1,14 +1,21 @@
 <?php get_header();
 
-  $page = $_GET['page'];
-  $offset = 0;
-  if($page) {
-    $offset = (intval($page)-1) * 12;
-  }
+  // get the page number from the url param which is called 'page'
+  $page = $_GET['page']
+  // if there is no url param, page number fallbacks to 1      
+  ?: 1;
+
+  // offset will be 12 times the page number, minus 12. For example: if the page number is 2, the offset will be 12. 
+  $offset = (intval($page)-1) * 12;
+  // url for fetching multiple pokemons. limit will always be 12. offset depends on the page number.
   $url = 'https://pokeapi.co/api/v2/pokemon?limit=12&offset=' . $offset;
+  // the body of the returned response.
   $response = json_decode(wp_remote_retrieve_body(wp_remote_get($url)));
+  // the pokemons array
   $results = $response->results;
+  // prints each pokemon in the array
   array_map('print_pokemon_card', $results);
+  // prints a navigation bar at the bottom
   print_nav_bar($page);
 ?>
   
